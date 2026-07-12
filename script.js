9827cd4a07c77bacefb73d30584ad5d225172dbc
varshini <varshini.vandanapu2@gmail.com>
2026-07-12 14:22:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("regForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // optional if you want custom validation first
+
+
+
+    window.location.href = "sucess.html";
+});
